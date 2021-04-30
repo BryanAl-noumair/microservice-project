0d20bb9d140402683929a1aabf5ec2f5081037e0
Bryan Al-noumair <>
2021-04-30 12:41:37 +0200

    create client and set up next into k8s

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,16 @@
+export default () => {
+  return (
+    <form>
+      <h1>Sign up</h1>
+      <div>
+        <label>Email Address</label>
+        <input />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" />
+      </div>
+      <button>Sign Up</button>
+    </form>
+  )
+}
